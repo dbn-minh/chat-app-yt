dd8ee362a826c79cd33f1e170b77c55c3e737080
dbn-minh <ititiu21243@student.hcmiu.edu.vn>
2024-12-26 22:35:32 +0700

    Done Backend and Create Frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,10 @@ export const getUsersForSideBar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
 
-        const filteredUsers = await User.find({})
+        // lấy thông tin tất cả người dùng, trừ người đăng nhập
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select('-password');
+
+        res.status(200).json(filteredUsers);
     } catch (err){
         console.log("Error in getUsersForSideBar controller: ", err.message);
         res.status(500).json({error: "Internal Server Error"});
