d071f56e8ac9973ec825d6eaf6e572cebb7dd14e
dbn-minh <ititiu21243@student.hcmiu.edu.vn>
2024-12-27 10:29:13 +0700

    Update Frontend: done login and signup, pending checkbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Login from "./pages/login/login.jsx";
+import Login from "./pages/login/Login.jsx";
+import Signup from "./pages/signup/Signup.jsx";
 
 function App() {
   return (
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Login/>
-
+        <Signup/>
       </div>
   )
 }
---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,6 @@
+const GenderCheckbox = () => {
+    return (
+        <div>GenderCheckbox</div>
+    )
+}
+export default GenderCheckbox
