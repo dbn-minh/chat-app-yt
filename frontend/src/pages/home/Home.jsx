2e88e151853d81e8b8e1ad7ac57d97084a7e7540
Đoàn Minh <ititiu21243@student.hcmiu.edu.vn>
2024-12-27 23:39:38 +0700

    Update Frontend: done Home, Sidebar, SearchInput

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import Sidebar from "../../components/Sidebar.jsx";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <Sidebar />
+            {/*<Message Container>*/}
+        </div>
     )
 }
 export default Home
