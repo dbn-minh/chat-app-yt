f9600a492b2f1c6ed6206b91e1823c23fa192427
dbn-minh <ititiu21243@student.hcmiu.edu.vn>
2024-12-30 14:14:52 +0700

    Frontend: Finish the UI, start to implement the APIs

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -19,3 +19,27 @@ const Messages = () => {
     )
 }
 export default Messages
+
+// STARTER CODE SNIPPET
+
+// import Message from "./Message.jsx";
+//
+// const Messages = () => {
+//     return (
+//         <div className='px-4 flex-1 overflow-auto'>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//             <Message/>
+//         </div>
+//     )
+// }
+// export default Messages
